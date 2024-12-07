8655c30f71e778b0ff0e073a30ee22c426ed4211
TairSara <tairtobol94@gmail.com>
2024-12-07 10:29:20 +0200

    create login and register

---
+++ DigireadProject/Models/ViewModels/ForgotPasswordViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DigireadProject.Models.ViewModels
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required(ErrorMessage = "נדרש אימייל")]
+        [EmailAddress(ErrorMessage = "אימייל לא תקין")]
+        [Display(Name = "אימייל")]
+        public string Email { get; set; }
+    }
+}
