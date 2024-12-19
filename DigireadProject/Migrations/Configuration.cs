a53401c3471eeeaca6ceffbbde1bdad2ddf1ed81
noamdahan1 <noam.dahan1998@gmail.com>
2024-12-19 15:19:33 +0200

    הוספת עמודת StockQuantityRent למודל הספרים

---
+++ DigireadProject/Migrations/Configuration.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
+namespace DigireadProject.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<DigireadProject.libraryProject_digireadEntities>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+    } 
+}
